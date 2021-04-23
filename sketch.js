4458523d2c227e8d793d43cfe443232e5b27d73f
Katherine Pierce <77492568+Vampire1K@users.noreply.github.com>
2021-04-23 18:04:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,14 +2,17 @@ var canvas, backgroundImage;
 
 var gameState = 0;
 var playerCount;
+var allPlayers;
 
 var database;
 
 var form, player, game;
 
+var car, car1, car2, car3, car4;
+
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth, displayHeight);
   database = firebase.database();
   game = new Game();
   game.getState();
