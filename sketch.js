9502114fd0a8b87935dff073000e6900b6d098c9
Katherine Pierce <77492568+Vampire1K@users.noreply.github.com>
2021-04-26 18:02:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,8 +8,20 @@ var database;
 
 var form, player, game;
 
-var car, car1, car2, car3, car4;
+var cars, car1, car2, car3, car4;
 
+var track, track2;
+
+var Car1Image, car2Image,  car3Image, car4Image;
+
+function preload(){
+  track = loadImage("../images/track.jpg")
+  track2 = loadImage("../images/track2.png");
+  car1 = loadImage("../images/car1.png");
+  car2 = loadImage("../images/car2.png");
+  car3 = loadImage("../images/car3.png");
+  car4 = loadImage("../images/car4.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth, displayHeight);
